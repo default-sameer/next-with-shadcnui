9e633e668b74f8b458ce69fc5dab3ea677324d7b
default-sameer <sameerjoshicr7@gmail.com>
2023-06-12 09:05:35 +0545

    Initial Commit

---
+++ config/site.ts
@@ -0,0 +1,20 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "with Shadcnui",
+  description:
+    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+    {
+      title: "About",
+      href: "/about",
+    },
+  ],
+  links: {
+    github: "https://github.com/default-sameer/next-with-shadcnui",
+  },
+}
